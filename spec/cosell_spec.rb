905aad9f873731f08683e346d1f6f3d49d847cfc
Steven Swerling <swerling@localhost.(none)>
2009-06-28 12:43:18 -0400

    got initial simple case working w/ test

--- spec/cosell_spec.rb
+++ spec/cosell_spec.rb
@@ -3,9 +3,9 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 #
 # A few mock announcments
 #
-class; AnnouncementMockA < Cosell::Announcement; end
-class; AnnouncementMockB < Cosell::Announcement; end
-class; AnnouncementMockC < AnnouncementMockB; end
+class AnnouncementA; end
+class AnnouncementB; end
+class AnnouncementC < AnnouncementB; end
 
 #
 # A class whose objects can act as announcers
@@ -21,24 +21,37 @@ describe Cosell do
     @announcer = AnyOldClass.new
   end
 
-  it "should announce instance of announcement class" do
-    # Implemenent
-    #   testAnnounceClass
-    #   testAnnounceInstance
+  it "should create announcement from class if needed" do
+    @announcer.announce(AnnouncementA).class.should == AnnouncementA
+    @announcer.announce(AnnouncementA.new).class.should == AnnouncementA
   end
 
   it "should execute block specified by subscription" do
-    # Implemenent
-    #   testAnnounceBlock
-  end
-
-  it "should execute method specified by subscription" do
-    # Implemenent
-    #   testSubscribeSend
+    
+    # Make sure the subscription block fires when an AnnouncementA is 
+    # announced, setting what_was_announced to the announcement)
+    what_was_announced = nil
+    @announcer.when_announcing(AnnouncementB) { |ann| what_was_announced = ann }
+    @announcer.announce AnnouncementB
+    what_was_announced.should_not be_nil
+    what_was_announced.class.should be_eql AnnouncementB
+   
+    what_was_announced = nil
+    @announcer.announce AnnouncementC
+    what_was_announced.should_not be_nil
+    what_was_announced.class.should be_eql AnnouncementC
   end
 
   it "should take actions only on announcements of events for which there is a subscribtion" do
+    # Make sure the subscription block fires when an AnnouncementA is 
+    # announced, setting what_was_announced to the announcement)
+    what_was_announced = nil
+    @announcer.when_announcing(AnnouncementB) { |ann| what_was_announced = ann }
+
+    @announcer.announce AnnouncementA
+    what_was_announced.should be_nil
     # Implemenent
+    # TODO:
     #   testSubscribeSet
     #   testSubscribeSubclass
   end
